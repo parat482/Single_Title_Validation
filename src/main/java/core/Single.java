055e9106fdad3c41706108c2366e451bd71ef7aa
Evgeny Osintsev <parat482@gmail.com>
2016-03-12 11:14:27 -0800

    third commit - variables

--- src/main/java/core/Single.java
+++ src/main/java/core/Single.java
@@ -9,10 +9,21 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class Single{
 	
 	public static void main(String[] args) {
+		
+		if (args.length == 0) {
+			System.err.println("No arguments");
+			System.exit(1);
+		}
+		
 		WebDriver driver = new FirefoxDriver();  // Version 1.1 :: Firefox
 //		WebDriver driver = new HtmlUnitDriver();       // Version 1.2 :: HtmlUnit
-		String url = "http://www.learn2test.net";
-		String title_expected = "learn2test.net";
+//		String url = "http://www.learn2test.net";
+//		String title_expected = "learn2test.net";
+		
+		String param[] = args[0].split("\\|");
+		String url = param[0];
+		String title_expected = param[1];
+		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		String title_actual = driver.getTitle();
